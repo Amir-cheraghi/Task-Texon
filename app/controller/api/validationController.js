1696562266b97bbad6364a51763865a4aed98062
Amir-cheraghi <chraghizadeh@gmail.com>
2021-07-04 20:05:33 +0430

    add login form validation

---
+++ app/controller/api/validationController.js
@@ -0,0 +1,32 @@
+const {check,validationResult} = require('express-validator')
+const {httpStatus} = require('./../../helpers/values')
+module.exports = new class validator{
+    loginCheck(req,res,next){
+        return [
+            check('username')
+            .notEmpty()
+            .withMessage('Please enter username')
+            .isLength({min : 6 , max : 32})
+            .withMessage('Username must be between 6 and 32 character'),
+            check('password')
+            .notEmpty()
+            .withMessage('Please enter password')
+            .isLength({min : 8 , max : 32})
+            .withMessage('password must be between 8 and 32 character'),            
+        ]
+    }
+
+    loginValidation(req,res,next){
+        const error = validationResult(req).array()
+        if(error.length){
+            const errorMessage = error.map(el=>{return el.msg})
+            res.status(httpStatus.badRequest).json({
+                status : 'Validation error',
+                msg : 'The entered data is not valid , please fix errors',
+                error : errorMessage
+            })
+            return next()
+        }
+        return next()
+    }
+}
--- app/routes/apiRoute.js
+++ app/routes/apiRoute.js
@@ -3,9 +3,14 @@ const authController = require('./../controller/api/authController')
 const routeController = require('./../controller/api/routeController')
 const userController = require('./../controller/api/userController')
 const adminController = require('./../controller/api/adminController')
+const validationController = require('./../controller/api/validationController')
+
 
 router.route('/index')
 .get(routeController.showIndex)
+
+router.route('/login')
+.post(validationController.loginCheck(),validationController.loginValidation)
 
 
 module.exports = router
