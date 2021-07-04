243836490a2c5a13406d120b8cc46b0411d58180
Amir-cheraghi <chraghizadeh@gmail.com>
2021-07-04 19:16:43 +0430

    create user model

---
+++ app/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username : {type : String , required : true , unique : true},
+    password : {type : String , required : true},
+    role : {type : Number , required : true , enum : [0,1] },
+    status : {type : String , required : true  },
+    spentTime : {type : String , required : true  },
+    estimatedTime : {type : String , required : true  },
+    sprint : {type : String , required : true  },
+    typeField : {type : String , required : true  }
+})
+
+const User = mongoose.model('User',userSchema)
+
+module.exports = User
