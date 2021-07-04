2fdca57ec84a7c48e16eaba252817515170f3957
Amir-cheraghi <chraghizadeh@gmail.com>
2021-07-04 19:34:08 +0430

    show user data in index of api

--- app/controller/api/routeController.js
+++ app/controller/api/routeController.js
@@ -1,3 +1,15 @@
+const {httpStatus} = require('./../../helpers/values')
+const User = require('./../../models/user')
 module.exports = new class routeController {
-    
+
+    async showIndex(req,res,next){
+        // 1- find user by id that in req.user
+        const data = await User.findById('60e1cbc96c98582ab4bd1f91')
+        //2- show user in response
+        res.status(httpStatus.success).json({
+            status : 'Success',
+            msg : `Dear ${data.username} , welcome to API , your data shown in below : `,
+            data
+        })
+    }
 }
--- app/routes/apiRoute.js
+++ app/routes/apiRoute.js
@@ -4,6 +4,8 @@ const routeController = require('./../controller/api/routeController')
 const userController = require('./../controller/api/userController')
 const adminController = require('./../controller/api/adminController')
 
+router.route('/index')
+.get(routeController.showIndex)
 
 
 module.exports = router
