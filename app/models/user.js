482c833ea1accaae419236275f9acaca3192a376
Amir-cheraghi <chraghizadeh@gmail.com>
2021-07-04 19:55:46 +0430

    refac : refac user model

--- app/models/user.js
+++ app/models/user.js
@@ -4,11 +4,17 @@ const userSchema = new mongoose.Schema({
     username : {type : String , required : true , unique : true},
     password : {type : String , required : true},
     role : {type : Number , required : true , enum : [0,1] },
-    status : {type : String , required : true  },
-    spentTime : {type : String , required : true  },
-    estimatedTime : {type : String , required : true  },
-    sprint : {type : String , required : true  },
-    typeField : {type : String , required : true  }
+    projects : [
+        {
+            status : {type : String , enum : ['There is a deadline' , 'Deadline has expired']  },
+            spentTime : {type : String},
+            estimatedTime : {type : Date},
+            sprint : {type : Number},
+            endingTimeBySprint : {type : Date},
+            typeField : {type : String , enum : ['emergency' , 'normal' , 'not important']}
+        }
+    ]
+    
 },{timestamps : true})
 
 const User = mongoose.model('User',userSchema)
