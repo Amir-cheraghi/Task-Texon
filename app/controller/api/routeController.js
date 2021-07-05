6b91622b8044f6a2cbe8ce3c3df32cba3f685e7b
Amir-cheraghi <chraghizadeh@gmail.com>
2021-07-05 17:31:21 +0430

    add feature calculate spent time and status in showing

--- app/controller/api/routeController.js
+++ app/controller/api/routeController.js
@@ -1,15 +1,33 @@
-const {httpStatus} = require('./../../helpers/values')
+const {httpStatus , projectStatus} = require('./../../helpers/values')
 const User = require('./../../models/user')
 module.exports = new class routeController {
 
     async showIndex(req,res,next){
         // 1- find user by id that in req.user
-        const data = await User.findById(req.user._id)
-        //2- show user in response
+        const user = await User.findById(req.user._id)
+        // 2- check having project or no to decide change time or not
+        if(user.projects.length){
+            const projectsId = user.projects.map(el=>el._id)
+
+            projectsId.forEach((el,i)=>{
+                const project = user.projects[i]
+
+                project.status = new Date(Date.now()).getTime() <  new Date(project.estimatedTime).getTime() ?  projectStatus.thereIsADeadLine : projectStatus.deadLineHasExpired
+
+                const difference =  new Date(Date.now()).getTime() - new Date(project.createdAt).getTime()
+                const day = difference / (24 * 3600 * 1000)
+                const hour =  (difference % (24 * 3600 * 1000)) / (1000*3600)
+                const min = ((difference % (24 * 3600 * 1000)) % (1000*3600)) / (1000*60)
+                project.spentTime = `${Math.floor(day)} Day and ${Math.floor(hour)} Hour and ${Math.floor(min)} Min` 
+            })
+            await user.save()
+        }
+
+        //- show user in response
         res.status(httpStatus.success).json({
             status : 'Success',
-            msg : `Dear ${data.username} , welcome to API , your data shown in below : `,
-            data
+            msg : `Dear ${user.username} , welcome to API , your data shown in below : `,
+            data : user._doc
         })
     }
 }
